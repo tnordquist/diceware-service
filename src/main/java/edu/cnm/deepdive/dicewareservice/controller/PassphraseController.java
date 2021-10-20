a1d3e3dd55732ca383f7c92f06a06c9334ab1783
Todd Nordquist <toddnordquist144@gmail.com>
2021-10-20 14:03:44 -0600

    Generated template version passphrase.

--- src/main/java/edu/cnm/deepdive/dicewareservice/controller/PassphraseController.java
+++ src/main/java/edu/cnm/deepdive/dicewareservice/controller/PassphraseController.java
@@ -2,12 +2,16 @@ package edu.cnm.deepdive.dicewareservice.controller;
 
 import edu.cnm.deepdive.dicewareservice.service.PassphraseGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/passphrases")
 public class PassphraseController {
 
@@ -19,8 +23,15 @@ public class PassphraseController {
     this.generator = generator;
   }
 
-  @GetMapping("/transient")
+  @ResponseBody
+  @GetMapping(value = "/transient", produces = MediaType.APPLICATION_JSON_VALUE)
   public String[] generate(@RequestParam(defaultValue = "5") int length) {
     return generator.generate(length);
+  }
+
+  @GetMapping(value = "/transient", produces = MediaType.TEXT_HTML_VALUE)
+  public String generate(@RequestParam(defaultValue = "5") int length, Model model) {
+    model.addAttribute("words", generator.generate(length));
+    return "passphrase";
   }
 }
--- src/main/java/edu/cnm/deepdive/dicewareservice/service/ResourceBundleProvider.java
+++ src/main/java/edu/cnm/deepdive/dicewareservice/service/ResourceBundleProvider.java
@@ -11,11 +11,7 @@ public class ResourceBundleProvider implements WordListProvider {
   private final List<String> words;
 
   public ResourceBundleProvider(ResourceBundle bundle) {
-    words = new LinkedList<>();
-    for (String key :
-        bundle.keySet()) {
-      words.add(bundle.getString(key));
-    }
+    words = new LinkedList<>(bundle.keySet());
   }
 
   @Override
